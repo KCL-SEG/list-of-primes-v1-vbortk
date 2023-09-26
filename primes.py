d019860ab50b4c9f14bc3da61e34cf91bfea8eca
Vytas <vboloki@gmail.com>
2023-09-26 18:48:39 +0100

    Completed Primes v1 exercise + gitignore

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 def primes(number_of_primes):
     list = []
+    x = 2
+    while number_of_primes > 0:
+        if is_prime(x):
+            list.append(x)
+            number_of_primes -= 1
+        x += 1
     return list
